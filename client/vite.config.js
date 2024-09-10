def21628e71c5aaeff732169b117674c951a590c
Nour Khansa <118853035+nourkhansa20@users.noreply.github.com>
2024-09-10 23:14:39 +0300

    fix the base url

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/alphaa/', // Set the base path for the project
   plugins: [react()],
 })
